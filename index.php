fb5f8d34ccf98649742df91840165fcf33f4312e
tomasz.bortacki <tomasz.bortacki99@gmail.com>
2022-03-13 11:33:17 +0100

    update nginx, php version and create html template for hello world

--- index.php
+++ index.php
@@ -1,3 +1,31 @@
 <?php
 
-echo 'Hi there 👋';
+$firstname = 'Tomasz';
+$lastname = 'Bortacki';
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta namespace='author' content='<?php echo $firstname.' '.$lastname ?>'>
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PAI - Lab1</title>
+    <style>
+        * {
+            margin: 0;
+            padding: 0;
+        }
+        
+        body {
+            padding: 1rem;
+            font-family: sans-serif;
+        }
+    </style>
+</head>
+<body>
+    <h1>Hello <?php echo $firstname.' '.$lastname ?>!</h1>
+</body>
+</html>
